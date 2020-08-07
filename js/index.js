8d1fcbbb255de96bc36621513f8f9305aeb8d448
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-07 23:12:08 +0300

    [SX-003] Video: structure

---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function() {
+    new vidbg('.video', {
+        mp4: 'video/world.mp4',
+        webm: 'video/world.webm',
+        poster: 'img/poster.jpg',
+        overlay: false
+      });
+})();

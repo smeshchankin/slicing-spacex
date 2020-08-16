1481ba85da563b0d2d66ccce2b59e148c86e6bbf
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-16 22:26:18 +0300

    [SX-009] Populator: footer navigation

--- js/data.js
+++ js/data.js
@@ -1,6 +1,8 @@
 (function() {
     populateList('.main-nav > .list > .item', ['Falcon 9', 'Falcon Heavy', 'Dragon', 'Updates']);
     populateList('.secondary-nav > .list > .item', ['About', 'Gallery', 'Shop']);
+    populateList('.footer-nav > .list > .item',
+        ['Twitter', 'Youtube', 'Instagram', 'Flickr', 'LinkedIn', 'Privacy', 'Policy']);
 
     function populateList(selector, list) {
         const node = document.querySelector(selector);

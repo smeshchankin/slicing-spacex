ca8f0c2e426475a7391267ba37741b83fa5be71e
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-17 23:51:59 +0300

    [SX-006][FIX] Rocket parallax: resize page

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 (function() {
+    const PARALLAX_SIZE = 560;
+
     new vidbg('.video', {
         mp4: 'video/world.mp4',
         webm: 'video/world.webm',
         poster: 'img/poster.jpg',
         overlay: false
-      });
+    });
 
-    if (document.body.clientWidth >= 576) {
-      new Rellax('.rocket');
+    let parallax = null;
+    resize();
+
+    window.addEventListener('resize', resize);
+
+    function resize() {
+        if (document.body.clientWidth < PARALLAX_SIZE) {
+            parallax.destroy();
+            parallax = null;
+        } else if (parallax == null) {
+        parallax = new Rellax('.rocket');
+      }
     }
 })();
